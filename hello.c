f911cf11a44af6782070642ddecaffc27ff952a5
Ben Fox-Moore <ben.foxmoore@gmail.com>
2013-05-02 18:16:13 +0100

    Initial tests

---
+++ hello.c
@@ -0,0 +1,32 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "gettext.h"
+
+int main(int argc, const char * argv[])
+{
+ const char *domain = "hello";
+ const char *directory = ".";
+ 
+ printf("Default LC_ALL: %s\n", setlocale(LC_ALL, ""));
+ printf("Default LC_COLLATE: %s\n", setlocale(LC_COLLATE, ""));
+ printf("Default LC_CTYPE: %s\n", setlocale(LC_CTYPE, ""));
+ printf("Default LC_MONETARY: %s\n", setlocale(LC_MONETARY, ""));
+ printf("Default LC_NUMERIC: %s\n", setlocale(LC_NUMERIC, ""));
+ printf("Default LC_TIME: %s\n", setlocale(LC_TIME, ""));
+
+ setlocale(LC_ALL, "it_IT");
+
+ printf("Using LC_ALL: %s\n", setlocale(LC_ALL, NULL));
+ printf("Using LC_COLLATE: %s\n", setlocale(LC_COLLATE, NULL));
+ printf("Using LC_CTYPE: %s\n", setlocale(LC_CTYPE, NULL));
+ printf("Using LC_MONETARY: %s\n", setlocale(LC_MONETARY, NULL));
+ printf("Using LC_NUMERIC: %s\n", setlocale(LC_NUMERIC, NULL));
+ printf("Using LC_TIME: %s\n", setlocale(LC_TIME, NULL));
+
+ bindtextdomain(domain, directory);
+ textdomain(domain);
+ printf( gettext( "Hello, world!\n" ) );
+
+ exit(0);
+}
